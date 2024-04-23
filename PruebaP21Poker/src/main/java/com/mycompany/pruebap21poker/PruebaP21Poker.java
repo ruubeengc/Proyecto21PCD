22d5c9f7e33f8d7fa79c10b0f0afff20f6dfc262
ruubeengc <gonzalez.147878@e.unavarra.es>
2024-04-23 08:47:47 +0200

    He añadido un nuevo sout para esclarecer todo

---
+++ PruebaP21Poker/src/main/java/com/mycompany/pruebap21poker/PruebaP21Poker.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebap21poker;
+
+/**
+ *
+ * @author alumno
+ */
+public class PruebaP21Poker {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Esto no deja de ser más que una prueba");
+    }
+}
